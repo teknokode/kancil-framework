af66e0633d7a24f755c9914d27650d34fb66cb08
Pino Ruswana <pinoruswana@gmail.com>
2025-08-05 22:19:02 +0700

    Menggunakan Medoo

--- system/Drivers/Mysql.php
+++ system/Drivers/Mysql.php
@@ -3,32 +3,46 @@
 namespace Kancil\Drivers;
 
 use Kancil\Interfaces\Database;
-//use Medoo\Medoo;
-use \PDO;
-use \PDOException;
+use Medoo\Medoo;
+
+// use \PDO;
+// use \PDOException;
 
 //class Mysql implements Database {
 class Mysql {
-    
-    public function connect() 
-    {
-        $dsn = "mysql:host=".DB_HOST.";dbname=".DB_NAME.";charset=utf8mb4";
-        $options = [
-            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            PDO::ATTR_EMULATE_PREPARES   => false,
-        ];
-        
-        try 
-        {
-            $this->db = new PDO( $dsn, DB_USER, DB_PASS, $options);
-            $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        
-        } catch(PDOException $e) {
 
-            pd("Connection failed: " . $e->getMessage());
-        }
+    private $db;
+
+    public function connect()
+    {
+        $this->db = new Medoo([
+            'type' => 'mariadb',
+            'host' => 'localhost',
+            'database' => 'name',
+            'username' => 'your_username',
+            'password' => 'your_password',
+        ]);
     }
+    
+    // public function connect() 
+    // {
+    //     $dsn = "mysql:host=".DB_HOST.";dbname=".DB_NAME.";charset=utf8mb4";
+    //     $options = [
+    //         PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    //         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    //         PDO::ATTR_EMULATE_PREPARES   => false,
+    //     ];
+        
+    //     try 
+    //     {
+    //         $this->db = new PDO( $dsn, DB_USER, DB_PASS, $options);
+    //         $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
+    //     } catch(PDOException $e) {
+
+    //         pd("Connection failed: " . $e->getMessage());
+    //     }
+    // }
 
 
     public function query( $sql )
@@ -46,19 +60,25 @@ class Mysql {
         return $stmt->fetchAll();
     }
 
-    public function get( $table ) 
-    {
-        return $this->query("SELECT * FROM $table");
-    }
-
-    // public function select( $table ) 
+    // public function get( $table ) 
     // {
     //     return $this->query("SELECT * FROM $table");
     // }
-    
+
+    public function get( $table ) 
+    {
+        return $this->db->select($table,"*");
+    }
+
+
     public function find( $table, $where ) 
     {
         return $this->query("SELECT * FROM $table WHERE $where");
+    }
+
+    public function where( $table, $column, $where ) 
+    {
+        return $this->where($table, $column, $where );
     }
 
     public function update() {}
